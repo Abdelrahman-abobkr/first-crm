e69c086fbb15f62e8a1cf1919df38315d66d977c
Abdelrahman-abobkr <abdelrahman16174@gmail.com>
2023-06-07 01:44:23 -0700

    Adding Password Reset Email Codes

--- crm6/settings.py
+++ crm6/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
 
 MEDIA_URL = '/media/'
@@ -121,5 +121,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EAMIL_USE_TLS = True
+EMAIL_HOST_USER = 'abdelrahmanboda164@gmail.com'
+EMAIL_HOST_PASSWORD = 'ccejbpiqxldxogpx'
 
 AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
